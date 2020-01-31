3aaa1420d98c770acba05672f995b9fcf2c6201a
Arthur Kazemi <bidadh@gmail.com>
2020-01-31 16:44:19 +1100

    add basic auth

--- kt-client/src/main/kotlin/com/ericsson/clc/samples/kt/openfeign/client/openfeign/PersonClient.kt
+++ kt-client/src/main/kotlin/com/ericsson/clc/samples/kt/openfeign/client/openfeign/PersonClient.kt
@@ -1,15 +1,24 @@
 package com.ericsson.clc.samples.kt.openfeign.client.openfeign
 
 import com.ericsson.clc.samples.kt.openfeign.client.model.Person
+import feign.auth.BasicAuthRequestInterceptor
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
-@FeignClient("kt-server")
+@FeignClient(name = "kt-server", configuration = [PersonClientConfiguration::class])
 interface PersonClient {
   @GetMapping("/all")
   fun fetchAll(): List<Person>
 
   @PostMapping("/save")
   fun save(person: Person): Person
+}
+
+class PersonClientConfiguration {
+  @Bean
+  fun basicAuthRequestInterceptor(): BasicAuthRequestInterceptor {
+    return BasicAuthRequestInterceptor("user", "pass")
+  }
 }
